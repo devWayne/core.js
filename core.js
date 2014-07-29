f6831373c5005905db9fe9ef399e64ba01a7895c
Wayne <dev.wayne.ix@gmail.com>
2014-07-29 16:20:29 +0800

    addfn

--- core.js
+++ core.js
@@ -1,33 +1,50 @@
 var core = (function() {
-	var core={};
-	$ = function(selector, context) {
+	var core = {};
+
+	var $ = function(selector, context) {
 		return core.init(selector, context)
-	}
+	};
+
+	$.fn = {
+		a: function() {
+			return console.log('hello');
+		}
+	};
+
 
 	core.init = function(selector, context) {
+		var dom;
 		if (!context) {
 			context = document;
 		}
 		if (!selector) {
 			return {}
 		} else {
-			return core.selectorng(context, selector);
+			dom = core.selectorng(context, selector);
 		}
-	}
+		dom.__proto__ = $.fn
+		//	dom.prototype=$.fn;
+		console.log(dom);
+		console.log(this);
+		return dom;
+	};
 
 	core.selectorng = function(elem, selector) {
 		var isId = selector[0] == "#",
 			isClass = selector[0] == ".",
 			selector_name = isId || isClass ? selector.slice(1) : selector;
-		if (isId){
+		if (isId) {
 			return elem.getElementsById(selector_name);
-	}
-	if (isClass) {
-		return elem.getElementsByClassName(selector_name);
-	} else {
-		return elem.getElementsByTagName(selector_name);
-	}
+		}
+		if (isClass) {
+			return elem.getElementsByClassName(selector_name);
+		} else {
+			return elem.getElementsByTagName(selector_name);
+		}
 
-}
-return $;
+	
+	};
+	//core.Z.prototype = $.fn;
+	return $;
 })()
+window.$ === undefined && (window.$ = core)
