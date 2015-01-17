25dc3db8372602ffcfe0b25c87b581963f47e5bc
Wayne <dev.wayne.ix@gmail.com>
2015-01-17 19:04:01 +0800

    add offset func

--- src/core.js
+++ src/core.js
@@ -98,6 +98,21 @@ var core = (function() {
                 if (this.parentNode != null)
                     this.parentNode.removeChild(this);
             })
+        },
+	
+	/**
+	 * Get offset
+	 * @return {void} description
+	 */
+        offset: function() {
+            if (this.length == 0) return null;
+            var obj = this[0].getBoundingClientRect();
+            return {
+                left: obj.left + window.pageXOffset,
+                top: obj.top + window.pageYOffset,
+                width: obj.width,
+                height: obj.height
+            };
         }
 
 
